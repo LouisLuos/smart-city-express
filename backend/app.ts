372b907d7b87c2124303a9b08a44e9c9a6102eac
Luiz Henrique <luiz.contato.1000@gmail.com>
2026-06-02 17:02:45 -0300

    feat(demand): Adiciona o método patch para edição de status de demanda.  - Zod de validação de tipo criado. - Service para processar se a alterção será validada. - Alteração do CORS para permitir métodos Patch. - Implementação na camada de Routes

--- backend/app.ts
+++ backend/app.ts
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(cors({
     origin: 'http://localhost:3000',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
 }));
 app.use(express.json({ limit: '10mb' }));
 
